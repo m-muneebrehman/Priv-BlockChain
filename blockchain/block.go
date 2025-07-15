0fa1574e7298a233efd0bd53e9988b307a072079
Muneeb <umuhammadmuneeb@gmail.com>
2025-07-15 15:39:44 +0500

    implementing the modular parts

---
+++ blockchain/block.go
@@ -0,0 +1,43 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *Blockchain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, newBlock)
+}
+
+func GenesisBlock() *Block {
+	return CreateBlock("Genesis Block", []byte{})
+}
+
+func InitializeBlockchain() *Blockchain {
+	genesis := GenesisBlock()
+	return &Blockchain{[]*Block{genesis}}
+}
--- main.go
+++ main.go
@@ -1,53 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"github.com/m-muneebrehman/Priv-BlockChain/blockchain"
 )
 
-type Blockchain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *Blockchain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
-}
-
-func GenesisBlock() *Block {
-	return CreateBlock("Genesis Block", []byte{})
-}
-
-func InitializeBlockchain() *Blockchain {
-	genesis := GenesisBlock()
-	return &Blockchain{[]*Block{genesis}}
-}
-
 func main() {
-	blockchain := InitializeBlockchain()
-	blockchain.AddBlock("First Block after Genesis")
-	blockchain.AddBlock("Second Block after Genesis")
-	for _, block := range blockchain.blocks {
+	chain := blockchain.InitializeBlockchain()
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	for _, block := range chain.Blocks {
 		fmt.Printf("Block Hash: %x\n", block.Hash)
 		fmt.Printf("Data: %s, PrevHash: %x\n", block.Data, block.PrevHash)
 	}
